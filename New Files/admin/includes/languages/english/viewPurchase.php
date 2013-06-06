6e22f391c02951c9ff63c11dd4d562b05c3d6484
Jordon <jordonkoh@hotmail.com>
2013-06-06 23:03:37 +0800

    pushing purchases addon on to git

---
+++ New Files/admin/includes/languages/english/viewPurchase.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+Sourcerers
+Copyright (c) 2013 Del, Jordon Koh, Low Guan Hong
+Released under the GNU General Public License Version 3
+
+This file is part of Sourcerers
+
+Sourcerers is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+A copy of the GNU General Public License is enclosed.
+************************************************************************
+For any enquiries, contact Del via email at magnadel@hotmail.com, Jordon Koh at jordonkoh@hotmail.com, or Low Guan Hong at grapefood@hotmail.com
+*/
+
+define('HEADING_TITLE', 'View Purchases');
+
+define('TABLE_HEADING_PURCHASES_ID', 'No.');
+define('TABLE_HEADING_DATE_OF_PURCHASE', 'Date Of Purchase');
+define('TABLE_HEADING_PRODUCT', 'Product Name');
+define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
+define('TABLE_HEADING_PRICE_PER_PIECE', 'Price Per Piece ($)');
+define('TABLE_HEADING_PURCHASE_QUANTITY', 'Quantity');
+define('TABLE_HEADING_SHIPPING_COST', 'Shipping Cost ($)');
+define('TABLE_HEADING_TOTAL_COST', 'Total Cost');
+define('TABLE_HEADING_AVERAGE_TOTAL_COST', 'Average Total Cost');
+define('TABLE_HEADING_PURCHASE_REMARKS', 'Purchase Remarks');
+
+?>
